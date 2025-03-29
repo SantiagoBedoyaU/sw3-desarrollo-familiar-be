69fbad5a01afdc2e858767d9d2b36def08aaa4f0
Santiago Bedoya Arcila <164806519+SantiagoBedoyaU@users.noreply.github.com>
2025-03-29 14:55:59 -0500

    Adding authentication (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,25 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
 import { ResearchArticle } from './research-articles/entities/research-article.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+      ],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
       }),
     }),
     HealthModule,
+    AuthModule,
     ResearchArticle,
   ],
   controllers: [],
---
+++ src/auth/users/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+export enum Roles {
+  Admin = 1,
+  Teacher,
+  Student,
+}
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ enum: Roles })
+  role: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
