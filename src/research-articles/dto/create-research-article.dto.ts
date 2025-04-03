85e626bfd72e4dbf222bdeba58791b2a387287df
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2025-04-02 21:15:28 -0500

    Adding validators for research articles & sign-in

--- src/research-articles/dto/create-research-article.dto.ts
+++ src/research-articles/dto/create-research-article.dto.ts
@@ -1,7 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateResearchArticleDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(4)
+  year: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   authors: string;
-  thematicAxis: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  primaryThematicAxis: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  secondaryThematicAxis: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  keywords: string;
+
+  @IsNotEmpty()
+  @IsString()
   summary: string;
   fileAddress?: string;
 }
