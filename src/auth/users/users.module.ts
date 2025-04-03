bec0e1a0604395af0635163930aeee316966c015
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2025-04-03 17:11:22 -0500

    Fixing issue

--- src/auth/users/users.module.ts
+++ src/auth/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
