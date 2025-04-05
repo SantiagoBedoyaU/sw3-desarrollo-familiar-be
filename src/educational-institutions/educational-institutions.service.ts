1ebf97249b731d637aa00f4dc1948409c79aa13e
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2025-04-05 15:40:21 -0500

    Remove unused import

--- src/educational-institutions/educational-institutions.service.ts
+++ src/educational-institutions/educational-institutions.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { EducationalInstitutionsRepository } from './educational-institutions.repository';
 import { BaseService } from 'src/shared/service/base-service';
 import { EducationalInstitution } from './entities/educational-institution.entity';
-import { FilterQuery } from 'mongoose';
 
 @Injectable()
 export class EducationalInstitutionsService extends BaseService<
