2631ff5ec9ef09d24186a61bcde9c238b13397e6
CristianDavGon <cristian.gonzalez22582@ucaldas,edu.co>
2025-04-11 13:42:54 -0500

    fix: wrong import

--- src/practice-reports/entities/practice-report.entity.ts
+++ src/practice-reports/entities/practice-report.entity.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 import { Types } from 'mongoose';
 
 export type PracticeReportDocument = HydratedDocument<PracticeReport>;
