771647436fa5a7914a53a240f34d608f9ddfd8b2
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2025-05-10 15:47:12 -0500

    Adding decorators

--- src/research-articles/dto/research-article-query-params.dto.ts
+++ src/research-articles/dto/research-article-query-params.dto.ts
@@ -1,10 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumberString, IsOptional, IsString } from 'class-validator';
 import { PaginationDto } from 'src/shared/dto/pagination.dto';
 
 export class ResearchArticleQueryParams extends PaginationDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
   title?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumberString()
   year?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
   authors?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
   primaryThematicAxis?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
   secondaryThematicAxis?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
   keywords?: string;
 }
--- src/shared/dto/pagination.dto.ts
+++ src/shared/dto/pagination.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional } from 'class-validator';
+
 export class PaginationDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
   page?: number = 1;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
   limit?: number = 10;
 }
