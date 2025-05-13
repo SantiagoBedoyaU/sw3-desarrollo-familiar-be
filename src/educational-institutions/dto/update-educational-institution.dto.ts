efc7704a057f6ace4b404258d596adb375a180ca
CristianDavGon <cristian.gonzalez22582@ucaldas,edu.co>
2025-05-12 21:58:28 -0500

    change in updateDto

--- src/educational-institutions/dto/update-educational-institution.dto.ts
+++ src/educational-institutions/dto/update-educational-institution.dto.ts
@@ -1,3 +1,6 @@
+import { PartialType } from '@nestjs/swagger';
 import { CreateEducationalInstitutionDto } from './create-educational-institution.dto';
 
-export class UpdateEducationalInstitutionDto extends CreateEducationalInstitutionDto {}
+export class UpdateEducationalInstitutionDto extends PartialType(
+  CreateEducationalInstitutionDto,
+) {}
